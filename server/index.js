7f3e29f7717696305900f31ab5510cb4d9b62d72
sava <savamedvedev@gmail.com>
2022-07-15 21:03:27 +0800

    first-commit

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import chalk from "chalk";
+import AuthRoute from "./Routes/AuthRoute";
+
+// Routes
+const app = express();
+
+// Middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+dotenv.config();
+
+const start = async () => {
+  try {
+    await mongoose
+      .connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((res) => console.log(chalk.bgBlue("DB Connect")))
+      .catch((err) => console.log(chalk.bgRed(err)));
+
+    app.listen(process.env.PORT, (err) => {
+      if (err) {
+        return console.log(chalk.bgRed(err));
+      }
+      console.log(chalk.bgBlue(`Starting Server on port ${process.env.PORT}`));
+    });
+  } catch (error) {
+    console.log(chalk.bgRed(error));
+  }
+};
+
+start();
+
+// usage of routes
+
+app.use("/auth", AuthRoute);
