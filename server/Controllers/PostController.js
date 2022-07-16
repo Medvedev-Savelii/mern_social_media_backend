2ca51622c59c875ac290b02d9fa516ae21a497fe
sava <savamedvedev@gmail.com>
2022-07-17 01:24:22 +0800

    seven-commit

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,33 +1,55 @@
+import PostModel from "../Models/postModel.js";
+import mongoose from "mongoose";
+import UserModel from "../Models/userModel.js";
+
+// Creat new Post
+
 export const createPost = async (req, res) => {
+  const newPost = new PostModel(req.body);
   try {
+    await newPost.save();
+    res.status(200).json("Post created!");
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+// Get a post
+
 export const getPost = async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+// Update a post
+
 export const updatePost = async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+// Delete a post
+
 export const deletePost = async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json(error);
   }
 };
+// like/dislike a post
+
 export const likePost = async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json(error);
   }
 };
+// Get Timeline POsts
+
 export const getTimelinePosts = async (req, res) => {
   try {
   } catch (error) {
