5a878341b3d2bee08b7445bcd5c8d6c76cdd76e6
sava <savamedvedev@gmail.com>
2022-07-17 02:44:04 +0800

    egtht-commit

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -17,7 +17,10 @@ export const createPost = async (req, res) => {
 // Get a post
 
 export const getPost = async (req, res) => {
+  const id = req.params.id;
   try {
+    const post = await PostModel.findById(id);
+    res.status(200).json(post);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -26,7 +29,17 @@ export const getPost = async (req, res) => {
 // Update a post
 
 export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { userId } = req.body;
+
   try {
+    const post = await PostModel.findById(postId);
+    if (post.userId === userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("Post Updated");
+    } else {
+      res.status(403).json("Action forbidden");
+    }
   } catch (error) {
     res.status(500).json(error);
   }
@@ -35,7 +48,16 @@ export const updatePost = async (req, res) => {
 // Delete a post
 
 export const deletePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
   try {
+    const post = await PostModel.findById(id);
+    if (post.userId === userId) {
+      await post.deleteOne();
+      res.status(200).json("POst deleted successfully");
+    } else {
+      res.status(403).json("Action forbidden");
+    }
   } catch (error) {
     res.status(500).json(error);
   }
@@ -43,11 +65,25 @@ export const deletePost = async (req, res) => {
 // like/dislike a post
 
 export const likePost = async (req, res) => {
+  const id = req.params.id;
+  const { userId } = req.body;
+
   try {
+    const post = await PostModel.findById(id);
+
+    if (!post.likes.includes(userId)) {
+      await post.updateOne({ $push: { likes: userId } });
+      res.status(200).json("Post liked");
+    } else {
+      await post.updateOne({ $pull: { likes: userId } });
+      res.status(200).json("Post Unliked");
+    }
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+
 // Get Timeline POsts
 
 export const getTimelinePosts = async (req, res) => {
