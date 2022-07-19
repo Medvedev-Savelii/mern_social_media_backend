e870a07ad7b82228dd430abbfb1a1e2e890bca6c
sava <savamedvedev@gmail.com>
2022-07-19 14:55:18 +0800

    2-commit-Integration

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -16,7 +16,13 @@ export const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    await newUser.save();
+    const user = await newUser.save();
+
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      process.env.JWTKEY,
+      { expiresIn: "1h" }
+    );
     res.status(200).json(newUser);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -32,11 +38,18 @@ export const loginUser = async (req, res) => {
     if (user) {
       const validity = await bcrypt.compare(password, user.password);
 
-      validity
-        ? res.status(200).json(user)
-        : res.status(400).json("Wrong Password");
+      if (!validity) {
+        res.status(400).json("Wrong Password");
+      } else {
+        const token = jwt.sign(
+          { username: user.username, id: user._id },
+          process.env.JWTKEY,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ user, token });
+      }
     } else {
-      res.status(404).json("User does not exists");
+      res.status(404).json("User not found");
     }
   } catch (error) {
     res.status(500).json({ message: error.message });
